3e50e046f71db827e31175a89a2ff94c602e7d6f
Emmanouil Kampitakis <info+git@kampitakis.de>
2017-03-28 22:10:16 +0000

    Added the flattening and deflattening of the matrix

--- image.go
+++ image.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/jpeg"
 	"log"
 	"os"
@@ -8,7 +9,7 @@ import (
 	"math"
 )
 
-func FromTwoD(x int, y int, YMax int) (int, error) {
+func From2D(x int, y int, YMax int) (int, error) {
 	if ( y >= YMax ) {
 		return 0, fmt.Errorf("Matrix Index y out of image bounds %d", y)
 	}
@@ -16,14 +17,14 @@ func FromTwoD(x int, y int, YMax int) (int, error) {
 	return x*YMax+y, nil
 }
 
-func ToTwoD(n int, YMax int) [2]int {
+func To2D(n int, YMax int) [2]int {
 	CalcY := math.Mod(float64(n),float64(YMax))
 	CalcX := (n-int(CalcY))/YMax
 
 	return [2]int{int(CalcX), int(CalcY)}
 }
 
-func img_to_array( image_path string ) []int {
+func GetImage( image_path string ) image.Image {
 	file, err := os.Open(image_path)
 	if err != nil {
 		log.Fatal(err)
@@ -35,30 +36,35 @@ func img_to_array( image_path string ) []int {
 	}
 	file.Close()
 
+	return img
+}
+
+func img_to_array( img image.Image ) []uint32 {
 	XImgSize := img.Bounds().Max.X - img.Bounds().Min.X
 	YImgSize := img.Bounds().Max.Y - img.Bounds().Min.Y
-
 	Size := XImgSize*YImgSize
 
-	var LinearImage = make([]int, Size)
+	var LinearImage = make([]uint32, Size)
 
 	for x:=0; x < XImgSize; x++ {
 		for y:=0; y < YImgSize; y++ {
 			colour := img.At(img.Bounds().Min.X+x,img.Bounds().Min.Y+y)
-			r,g,b,_ := colour.RGBA()
-			pos, err := FromTwoD(x,y,YImgSize)
+			r,b,g,_ := colour.RGBA()
+			pos, err := From2D(x,y,YImgSize)
 			if err != nil {
 				log.Fatal(err)
 			}
-			LinearImage[pos] = int((r+g+b)/3)
+			LinearImage[pos] = uint32(r/3+b/3+g/3)
 		}
 	}
-
 	return LinearImage
 }
 
 func main() {
 	in_image_path := os.Args[1]
-	ImageMatrix := img_to_array(in_image_path)
-	fmt.Println("%d", ImageMatrix[70])
+	img := GetImage(in_image_path)
+	ImageMatrix := img_to_array(img)
+	for i:=0; i<len(ImageMatrix); i++ {
+		fmt.Println(To2D(i, img.Bounds().Max.Y))
+	}
 }
